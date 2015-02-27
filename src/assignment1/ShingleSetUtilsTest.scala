56480cfc6ba511175ac3f83e31e4fb63ca5a5242
KolakCC <KolakCC@gmail.com>
2015-02-27 14:04:05 +0100

    added tests

--- src/assignment1/ShingleSetUtilsTest.scala
+++ src/assignment1/ShingleSetUtilsTest.scala
@@ -3,21 +3,43 @@ package assignment1
 import assignment1.samples.ShingleSet
 import org.scalatest.FunSuite
 
-class Test extends FunSuite {
-  test("ShingleSet") {
-    val s = new ShingleSet(2)
-    s.add("ab")
-    s.add("bc")
-    s.add("cd")
-    s.add("da")
-    s.add("bd")
-    assertResult({
+class ShingleSetUtilsTest extends FunSuite {
+  test("ShingleSet example in the assignment") {
+    val expected = new ShingleSet(2)
+    expected.add("ab")
+    expected.add("bc")
+    expected.add("cd")
+    expected.add("da")
+    expected.add("bd")
 
-      s
-    }
-    ) {val s = new ShingleSet(2)
-      ShingleSetUtils.createShingles(s, "abcdabd",2)
-      s
-    }
+    val result = new ShingleSet(2)
+    ShingleSetUtils.createShingles(result, "abcdabd", 2)
+
+    assertResult(expected) { result }
+  }
+
+  test("ShingleSet self made k=5") {
+    val expected = new ShingleSet(5)
+    expected.add("abcda")
+    expected.add("bcdab")
+    expected.add("cdadd")
+
+    val result = new ShingleSet(5)
+    ShingleSetUtils.createShingles(result, "abcdabd", 5)
+
+    assertResult(expected) { result }
+  }
+
+  test("ShingleSet self made k=1") {
+    val expected = new ShingleSet(5)
+    expected.add("a")
+    expected.add("b")
+    expected.add("c")
+    expected.add("d")
+
+    val result = new ShingleSet(1)
+    ShingleSetUtils.createShingles(result, "abcdabd", 1)
+
+    assertResult(expected) { result }
   }
 }
