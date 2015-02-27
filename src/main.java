f097afad3ca07ed3b3993db83ef1ddbbc6e889a8
KolakCC <KolakCC@gmail.com>
2015-02-27 14:42:55 +0100

    implemented plotting

--- src/main.java
+++ src/main.java
@@ -2,6 +2,14 @@
 public class main {
 
 	public static void perceptron() {
+        Dataset dataset = new Dataset("data/guassian.txt", true);
+        Perceptron perceptron = new Perceptron(1.0d);
+        for (FeatureVector featureVector : dataset) {
+            perceptron.train(featureVector);
+        }
+        PerceptronPlotter perceptronPlotter = new PerceptronPlotter("first", "second");
+        perceptronPlotter.plotData(dataset, perceptron);
+
 		// add code here
 	}
 
