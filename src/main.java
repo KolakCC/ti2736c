a9382e6c4489ebe73999e14eb48eb8c85802a17f
KolakCC <KolakCC@gmail.com>
2015-03-06 14:12:39 +0100

    assignment 3

--- src/main.java
+++ src/main.java
@@ -1,79 +1,45 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 
 public class main {
 
-	public static void perceptron() {
-        Dataset dataset = new Dataset("data/gaussian.txt", true);
-        Perceptron perceptron = new Perceptron(1.0d);
-        PerceptronPlotter perceptronPlotter = new PerceptronPlotter("first", "second");
-
-        for (FeatureVector featureVector : dataset) {
-            perceptron.train(featureVector);
-        }
-        perceptronPlotter.plotData(dataset, perceptron);
-
-
-		// add code here
-	}
-
-	public static void perceptronDigits() {
-		DigitFrame digitFrame = new DigitFrame("Perceptron");
-        Dataset dataset = new Dataset("data/train_digits.txt", true);
-
-        Perceptron perceptron = new Perceptron(1d);
-        perceptron.updateWeights(dataset);
-        perceptron.updateWeights(dataset);
-        //perceptron.updateWeights(dataset);
-
-        Dataset testDataset = new Dataset("data/test_digits.txt", true);
-        int misclassified = 0;
-        for (FeatureVector fv : testDataset) {
-            int predicted = perceptron.predict(fv);
-            if (predicted != fv.getLabel()) {
-                new DigitFrame("Misclassified " + misclassified, fv, 8, 8);
-                misclassified++;
-            }
-        }
-
-        System.out.println("Miss rate: " + ((double) misclassified / (double) testDataset.size()));
-
-        digitFrame.showImage(perceptron.getWeights(), 8, 8);
-	}
-
-	public static void nearestNeighbour() {
-		// add code here
-        int k = 3;
-        NearestNeighbour nearestNeighbour = new NearestNeighbour();
-        nearestNeighbour.readData("data/banana.txt");
-        NearestNeighbourPlotter nearestNeighbourPlotter = new NearestNeighbourPlotter(k);
-        nearestNeighbourPlotter.plotData(nearestNeighbour);
-
-	}
-	
-	public static void nearestNeighbourDigits() {
-
-        int k = 3;
-        NearestNeighbour nearestNeighbour = new NearestNeighbour();
-        //NearestNeighbourPlotter nearestNeighbourPlotter = new NearestNeighbourPlotter(k);
-        nearestNeighbour.readData("data/train_digits.txt");
-        int misclassified = 0;
-        for (FeatureVector fv : nearestNeighbour.dataset) {
-            int predicted = nearestNeighbour.predict(fv, k);
-            if (predicted != fv.getLabel()) {
-                misclassified++;
-                System.out.println("Misclassified " + misclassified);
-            }
-        }
-
-        System.out.println("Miss rate: " + ((double) misclassified / (double) nearestNeighbour.dataset.size()));
-
-        //nearestNeighbourPlotter.plotData(nearestNeighbour);
-	}
-
 	public static void main(String[] args) {
-		//perceptron();
-		perceptronDigits();
-		//nearestNeighbour();
-		//nearestNeighbourDigits();
+
+        // FILL IN YOUR CODE HERE
+
+	}
+
+	/*
+	 * Java method to sort Map in Java by value e.g. HashMap or Hashtable
+	 * throw NullPointerException if Map contains null values
+	 * It also sort values even if they are duplicates
+	 */
+	public static <K extends Comparable,V extends Comparable> Map<K,V> sortByValues(Map<K,V> map){
+		List<Map.Entry<K,V>> entries = new LinkedList<Map.Entry<K,V>>(map.entrySet());
+
+		Collections.sort(entries, new Comparator<Map.Entry<K,V>>() {
+
+			@Override
+			public int compare(Entry<K, V> o1, Entry<K, V> o2) {
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+
+		//LinkedHashMap will keep the keys in the order they are inserted
+		//which is currently sorted on natural ordering
+		Map<K,V> sortedMap = new LinkedHashMap<K,V>();
+
+		for(Map.Entry<K,V> entry: entries){
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+
+		return sortedMap;
 	}
 
 }
