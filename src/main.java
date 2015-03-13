1e4fdc09a9a87c37030d2304f97f85398d0e8536
KolakCC <KolakCC@gmail.com>
2015-03-13 14:57:18 +0100

    added assignment 4

--- src/main.java
+++ src/main.java
@@ -1,54 +1,8 @@
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 
 public class main {
 
 	public static void main(String[] args) {
-        //PageRank pageRank = new PageRank();
-        TaxationPageRank pageRank = new TaxationPageRank(0.8d);
-        pageRank.importData("data/flight_Data.txt");
-        //pageRank.importData("data/example2.txt");
-        //System.out.println(pageRank.data);
-
-        //pageRank.constructTransitionMatrix();
-
-        //pageRank.getRandomSurferVector();
-
-        Map<String, Double> stringDoubleMap = pageRank.calculatePageRank(10);
-        System.out.println(sortByValues(stringDoubleMap));
-    }
-
-	/*
-	 * Java method to sort Map in Java by value e.g. HashMap or Hashtable
-	 * throw NullPointerException if Map contains null values
-	 * It also sort values even if they are duplicates
-	 */
-	public static <K extends Comparable,V extends Comparable> Map<K,V> sortByValues(Map<K,V> map){
-		List<Map.Entry<K,V>> entries = new LinkedList<Map.Entry<K,V>>(map.entrySet());
-
-		Collections.sort(entries, new Comparator<Map.Entry<K, V>>() {
-
-            @Override
-            public int compare(Entry<K, V> o1, Entry<K, V> o2) {
-                return o2.getValue().compareTo(o1.getValue());
-            }
-        });
-
-		//LinkedHashMap will keep the keys in the order they are inserted
-		//which is currently sorted on natural ordering
-		Map<K,V> sortedMap = new LinkedHashMap<K,V>();
-
-		for(Map.Entry<K,V> entry: entries){
-			sortedMap.put(entry.getKey(), entry.getValue());
-		}
-
-		return sortedMap;
+		// add code here
 	}
 
 }
